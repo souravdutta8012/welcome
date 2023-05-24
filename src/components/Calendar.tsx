3e51e3597216bbe1633229f2fd3b39c684b4a4cf
souravdutta8012 <souravdutta2108@gmail.com>
2023-05-24 18:29:54 +0530

    changes

--- src/components/Calendar.tsx
+++ src/components/Calendar.tsx
@@ -2,23 +2,22 @@ import { Box } from "@mui/material";
 import { getLight } from "../util/Function";
 const obj = [14, 15, 16, 17, 18];
 export const Calendar = () => {
-  const light = getLight();
-  return (
-    <>
-      <Box className={light ? "Backdrop-light" : "Backdrop"}></Box>
-      <Box className="Grad1"></Box>
-      <Box className="Grad2 overflow-clip"></Box>
-
-      <Box className=" flex justify-center items-center  py-8">
-        {obj.map((item: any) => {
-          return (
-            <Box className="w-20 h-20 border border-solid border-red-50 bg-green flex flex-col justify-center items-center">
-              <Box className="text-4xl font-bold text-white">{item}</Box>
-              <Box className="text-2xl font-bold text-white">JUN</Box>
-            </Box>
-          );
-        })}
-      </Box>
-    </>
-  );
+	const light = getLight();
+	return (
+		<>
+			<Box className={light ? "Backdrop-light" : "Backdrop"}></Box>
+			<Box className="Grad1"></Box>
+			<Box className="Grad2 overflow-clip"></Box>
+			<Box className=" flex justify-center items-center  py-8">
+				{obj.map((item: any) => {
+					return (
+						<Box className="w-20 h-20 border border-solid border-red-50 bg-green flex flex-col justify-center items-center">
+							<Box className="text-4xl font-bold text-white">{item}</Box>
+							<Box className="text-2xl font-bold text-white">JUN</Box>
+						</Box>
+					);
+				})}
+			</Box>
+		</>
+	);
 };
