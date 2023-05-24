39709e3ad74f0d3ff40952570b0eeb0a002ea1cf
souravdutta8012 <souravdutta2108@gmail.com>
2023-05-24 18:26:52 +0530

    changes

--- src/Router.tsx
+++ src/Router.tsx
@@ -12,7 +12,6 @@ export const Router = (_props: any) => {
             <Route path="/" element={<Navigate to="/welcome" />} />
             <Route path="/welcome/:name?" element={<Welcome />} />
             <Route path="/welcome/home/:name?" element={<Home />} />
-            {/* <Route path="/welcome/calender" element={<Calendar />} /> */}
           </Routes>
         </Box>
       </BrowserRouter>
