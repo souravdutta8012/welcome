41e003de894b7750fa155538366e007d4a8e3ee3
souravdutta8012 <souravdutta2108@gmail.com>
2023-05-25 11:11:37 +0530

    changes

--- src/components/Back.tsx
+++ src/components/Back.tsx
@@ -5,7 +5,7 @@ export default function Back(props: any) {
 
     return (
         <>
-            <Box className="absolute bottom-5 w-full px-6">
+            <Box className="sticky bottom-5 w-full px-6">
                 <Button
                     variant="contained"
                     className={"w-full h-14 font-semibold text-xl capitalize rounded-lg bg-gradient-to-r " + (light ? "from-secondary-purple to-primary-pink" : "from-secondary-blue to-secondary-purple")} size="large"
