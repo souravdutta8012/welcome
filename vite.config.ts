6664a5f1fc3dba5838e8f91199b79c9cce3be457
souravdutta8012 <souravdutta2108@gmail.com>
2023-05-27 14:08:38 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,37 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { VitePWA } from 'vite-plugin-pwa'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'test.png'],
+      manifest: {
+        name: 'Welcome',
+        short_name: 'Welcome',
+        description: 'Welcome',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'test.png',
+            sizes: '144x144',
+            type: 'image/png'
+          },
+          {
+            src: 'test.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'test.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any muskable'
+          }
+        ]
+      }
+    })
+  ],
   build: {
     outDir: './build'
   }
