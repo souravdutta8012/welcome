fb7e11b0a33e2ac4c4e98e0af0e0c9efe02e5ab9
souravdutta8012 <souravdutta2108@gmail.com>
2023-05-27 15:43:20 +0530

    changes

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -9,13 +9,13 @@ import { getLight } from "../util/Function";
 export default function Home() {
 	const [open, setopen] = useState(false);
 	const [value, setvalue] = useState(undefined);
-	const [height, setHeight] = useState(window.innerHeight - 360);
+	const [height, setHeight] = useState(window.innerHeight - 350);
 
 	const { auth } = useUserState();
 	const light = getLight();
 
 	useEffect(() => {
-		const handleWindowResize = () => setHeight(window.innerHeight - 360);
+		const handleWindowResize = () => setHeight(window.innerHeight - 350);
 		window.addEventListener("resize", handleWindowResize);
 		return () => window.removeEventListener("resize", handleWindowResize);
 	}, []);
@@ -66,6 +66,265 @@ export default function Home() {
 									</Box>
 									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
 										View Program
+									</Box>
+								</Box>
+							</Box>
+						</Box>
+					</Box>
+					<Box>
+						<Box className={"text-center font-semibold text-lg tracking-widest pb-2 " + (light ? "text-secondary-gray" : "text-secondary-gray")}>
+							Profile Connect
+						</Box>
+						<Box className="flex justify-around items-center pt-2 pb-8">
+							<Box>
+								<Box className={"w-32 h-32 rounded-lg bg-gradient-to-bl " + (light ? "from-secondary-blue to-primary-blue" : "from-primary-pink to-primary-blue")}>
+									<div className="flex w-full h-full justify-center items-center" onClick={() => openDialog('contact')}>
+										<img src="/contact.svg" className={"" + (light ? "text-primary-light" : "text-primary-light")} />
+									</div>
+								</Box>
+								<Box className="text-center pt-2">
+									<Box className={"font-semibold " + (light ? "text-primary-dark" : "text-primary-light")}>
+										Contact
+									</Box>
+									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
+										TCS Leadership
+									</Box>
+								</Box>
+							</Box>
+							<Box>
+								<Box className={"w-32 h-32 rounded-lg bg-gradient-to-bl " + (light ? "from-secondary-blue to-primary-blue" : "from-primary-pink to-primary-blue")}>
+									<div className="flex w-full h-full justify-center items-center" onClick={() => openDialog('office')}>
+										<img src="/office.svg" className={"" + (light ? "text-primary-light" : "text-primary-light")} />
+									</div>
+								</Box>
+								<Box className="text-center pt-2">
+									<Box className={"font-semibold " + (light ? "text-primary-dark" : "text-primary-light")}>
+										Pune Office
+									</Box>
+									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
+										TCS Pune Office
+									</Box>
+								</Box>
+							</Box>
+						</Box>
+					</Box>
+					<Box>
+						<Box className={"text-center font-semibold text-lg tracking-widest pb-2 " + (light ? "text-secondary-gray" : "text-secondary-gray")}>
+							Profile Connect
+						</Box>
+						<Box className="flex justify-around items-center pt-2 pb-8">
+							<Box>
+								<Box className={"w-32 h-32 rounded-lg bg-gradient-to-bl " + (light ? "from-secondary-blue to-primary-blue" : "from-primary-pink to-primary-blue")}>
+									<div className="flex w-full h-full justify-center items-center" onClick={() => openDialog('contact')}>
+										<img src="/contact.svg" className={"" + (light ? "text-primary-light" : "text-primary-light")} />
+									</div>
+								</Box>
+								<Box className="text-center pt-2">
+									<Box className={"font-semibold " + (light ? "text-primary-dark" : "text-primary-light")}>
+										Contact
+									</Box>
+									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
+										TCS Leadership
+									</Box>
+								</Box>
+							</Box>
+							<Box>
+								<Box className={"w-32 h-32 rounded-lg bg-gradient-to-bl " + (light ? "from-secondary-blue to-primary-blue" : "from-primary-pink to-primary-blue")}>
+									<div className="flex w-full h-full justify-center items-center" onClick={() => openDialog('office')}>
+										<img src="/office.svg" className={"" + (light ? "text-primary-light" : "text-primary-light")} />
+									</div>
+								</Box>
+								<Box className="text-center pt-2">
+									<Box className={"font-semibold " + (light ? "text-primary-dark" : "text-primary-light")}>
+										Pune Office
+									</Box>
+									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
+										TCS Pune Office
+									</Box>
+								</Box>
+							</Box>
+						</Box>
+					</Box>
+					<Box>
+						<Box className={"text-center font-semibold text-lg tracking-widest pb-2 " + (light ? "text-secondary-gray" : "text-secondary-gray")}>
+							Profile Connect
+						</Box>
+						<Box className="flex justify-around items-center pt-2 pb-8">
+							<Box>
+								<Box className={"w-32 h-32 rounded-lg bg-gradient-to-bl " + (light ? "from-secondary-blue to-primary-blue" : "from-primary-pink to-primary-blue")}>
+									<div className="flex w-full h-full justify-center items-center" onClick={() => openDialog('contact')}>
+										<img src="/contact.svg" className={"" + (light ? "text-primary-light" : "text-primary-light")} />
+									</div>
+								</Box>
+								<Box className="text-center pt-2">
+									<Box className={"font-semibold " + (light ? "text-primary-dark" : "text-primary-light")}>
+										Contact
+									</Box>
+									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
+										TCS Leadership
+									</Box>
+								</Box>
+							</Box>
+							<Box>
+								<Box className={"w-32 h-32 rounded-lg bg-gradient-to-bl " + (light ? "from-secondary-blue to-primary-blue" : "from-primary-pink to-primary-blue")}>
+									<div className="flex w-full h-full justify-center items-center" onClick={() => openDialog('office')}>
+										<img src="/office.svg" className={"" + (light ? "text-primary-light" : "text-primary-light")} />
+									</div>
+								</Box>
+								<Box className="text-center pt-2">
+									<Box className={"font-semibold " + (light ? "text-primary-dark" : "text-primary-light")}>
+										Pune Office
+									</Box>
+									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
+										TCS Pune Office
+									</Box>
+								</Box>
+							</Box>
+						</Box>
+					</Box>
+					<Box>
+						<Box className={"text-center font-semibold text-lg tracking-widest pb-2 " + (light ? "text-secondary-gray" : "text-secondary-gray")}>
+							Profile Connect
+						</Box>
+						<Box className="flex justify-around items-center pt-2 pb-8">
+							<Box>
+								<Box className={"w-32 h-32 rounded-lg bg-gradient-to-bl " + (light ? "from-secondary-blue to-primary-blue" : "from-primary-pink to-primary-blue")}>
+									<div className="flex w-full h-full justify-center items-center" onClick={() => openDialog('contact')}>
+										<img src="/contact.svg" className={"" + (light ? "text-primary-light" : "text-primary-light")} />
+									</div>
+								</Box>
+								<Box className="text-center pt-2">
+									<Box className={"font-semibold " + (light ? "text-primary-dark" : "text-primary-light")}>
+										Contact
+									</Box>
+									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
+										TCS Leadership
+									</Box>
+								</Box>
+							</Box>
+							<Box>
+								<Box className={"w-32 h-32 rounded-lg bg-gradient-to-bl " + (light ? "from-secondary-blue to-primary-blue" : "from-primary-pink to-primary-blue")}>
+									<div className="flex w-full h-full justify-center items-center" onClick={() => openDialog('office')}>
+										<img src="/office.svg" className={"" + (light ? "text-primary-light" : "text-primary-light")} />
+									</div>
+								</Box>
+								<Box className="text-center pt-2">
+									<Box className={"font-semibold " + (light ? "text-primary-dark" : "text-primary-light")}>
+										Pune Office
+									</Box>
+									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
+										TCS Pune Office
+									</Box>
+								</Box>
+							</Box>
+						</Box>
+					</Box>
+					<Box>
+						<Box className={"text-center font-semibold text-lg tracking-widest pb-2 " + (light ? "text-secondary-gray" : "text-secondary-gray")}>
+							Profile Connect
+						</Box>
+						<Box className="flex justify-around items-center pt-2 pb-8">
+							<Box>
+								<Box className={"w-32 h-32 rounded-lg bg-gradient-to-bl " + (light ? "from-secondary-blue to-primary-blue" : "from-primary-pink to-primary-blue")}>
+									<div className="flex w-full h-full justify-center items-center" onClick={() => openDialog('contact')}>
+										<img src="/contact.svg" className={"" + (light ? "text-primary-light" : "text-primary-light")} />
+									</div>
+								</Box>
+								<Box className="text-center pt-2">
+									<Box className={"font-semibold " + (light ? "text-primary-dark" : "text-primary-light")}>
+										Contact
+									</Box>
+									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
+										TCS Leadership
+									</Box>
+								</Box>
+							</Box>
+							<Box>
+								<Box className={"w-32 h-32 rounded-lg bg-gradient-to-bl " + (light ? "from-secondary-blue to-primary-blue" : "from-primary-pink to-primary-blue")}>
+									<div className="flex w-full h-full justify-center items-center" onClick={() => openDialog('office')}>
+										<img src="/office.svg" className={"" + (light ? "text-primary-light" : "text-primary-light")} />
+									</div>
+								</Box>
+								<Box className="text-center pt-2">
+									<Box className={"font-semibold " + (light ? "text-primary-dark" : "text-primary-light")}>
+										Pune Office
+									</Box>
+									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
+										TCS Pune Office
+									</Box>
+								</Box>
+							</Box>
+						</Box>
+					</Box>
+					<Box>
+						<Box className={"text-center font-semibold text-lg tracking-widest pb-2 " + (light ? "text-secondary-gray" : "text-secondary-gray")}>
+							Profile Connect
+						</Box>
+						<Box className="flex justify-around items-center pt-2 pb-8">
+							<Box>
+								<Box className={"w-32 h-32 rounded-lg bg-gradient-to-bl " + (light ? "from-secondary-blue to-primary-blue" : "from-primary-pink to-primary-blue")}>
+									<div className="flex w-full h-full justify-center items-center" onClick={() => openDialog('contact')}>
+										<img src="/contact.svg" className={"" + (light ? "text-primary-light" : "text-primary-light")} />
+									</div>
+								</Box>
+								<Box className="text-center pt-2">
+									<Box className={"font-semibold " + (light ? "text-primary-dark" : "text-primary-light")}>
+										Contact
+									</Box>
+									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
+										TCS Leadership
+									</Box>
+								</Box>
+							</Box>
+							<Box>
+								<Box className={"w-32 h-32 rounded-lg bg-gradient-to-bl " + (light ? "from-secondary-blue to-primary-blue" : "from-primary-pink to-primary-blue")}>
+									<div className="flex w-full h-full justify-center items-center" onClick={() => openDialog('office')}>
+										<img src="/office.svg" className={"" + (light ? "text-primary-light" : "text-primary-light")} />
+									</div>
+								</Box>
+								<Box className="text-center pt-2">
+									<Box className={"font-semibold " + (light ? "text-primary-dark" : "text-primary-light")}>
+										Pune Office
+									</Box>
+									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
+										TCS Pune Office
+									</Box>
+								</Box>
+							</Box>
+						</Box>
+					</Box>
+					<Box>
+						<Box className={"text-center font-semibold text-lg tracking-widest pb-2 " + (light ? "text-secondary-gray" : "text-secondary-gray")}>
+							Profile Connect
+						</Box>
+						<Box className="flex justify-around items-center pt-2 pb-8">
+							<Box>
+								<Box className={"w-32 h-32 rounded-lg bg-gradient-to-bl " + (light ? "from-secondary-blue to-primary-blue" : "from-primary-pink to-primary-blue")}>
+									<div className="flex w-full h-full justify-center items-center" onClick={() => openDialog('contact')}>
+										<img src="/contact.svg" className={"" + (light ? "text-primary-light" : "text-primary-light")} />
+									</div>
+								</Box>
+								<Box className="text-center pt-2">
+									<Box className={"font-semibold " + (light ? "text-primary-dark" : "text-primary-light")}>
+										Contact
+									</Box>
+									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
+										TCS Leadership
+									</Box>
+								</Box>
+							</Box>
+							<Box>
+								<Box className={"w-32 h-32 rounded-lg bg-gradient-to-bl " + (light ? "from-secondary-blue to-primary-blue" : "from-primary-pink to-primary-blue")}>
+									<div className="flex w-full h-full justify-center items-center" onClick={() => openDialog('office')}>
+										<img src="/office.svg" className={"" + (light ? "text-primary-light" : "text-primary-light")} />
+									</div>
+								</Box>
+								<Box className="text-center pt-2">
+									<Box className={"font-semibold " + (light ? "text-primary-dark" : "text-primary-light")}>
+										Pune Office
+									</Box>
+									<Box className={"text-sm " + (light ? "text-secondary-purple" : "text-secondary-gray")}>
+										TCS Pune Office
 									</Box>
 								</Box>
 							</Box>
