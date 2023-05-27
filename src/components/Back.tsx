b81a7940a389ce5ee3c69362324a486142a13c42
souravdutta8012 <souravdutta2108@gmail.com>
2023-05-27 21:20:30 +0530

    changes

--- src/components/Back.tsx
+++ src/components/Back.tsx
@@ -8,7 +8,7 @@ export default function Back(props: any) {
 
     return (
         <>
-            <Box className={"absolute bottom-0 w-full px-6 pb-5 bg-gradient-to-t " + (light ? "from-primary-light" : "from-primary-dark")}>
+            <Box className={"absolute bottom-0 w-full px-6 pb-5 bg-gradient-to-t z-50 " + (light ? "from-primary-light" : "from-primary-dark")}>
                 <Button
                     variant="contained"
                     className={"w-full h-14 font-semibold text-xl capitalize rounded-lg bg-gradient-to-r " + (light ? "from-secondary-purple to-primary-pink" : "from-secondary-blue to-secondary-purple")} size="large"
