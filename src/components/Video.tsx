a7c1a6d29bec140131a71eaab2f1393f76183bf1
souravdutta8012 <souravdutta2108@gmail.com>
2023-05-30 16:16:43 +0530

    changes

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,6 +1,5 @@
 import { useEffect, useRef, useState } from "react";
 import { useUserState } from "../util/User";
-import { Card, CardCover } from "@mui/joy";
 
 export default function Video() {
     const vidRef = useRef<any>(null);
@@ -30,25 +29,21 @@ export default function Video() {
 
     return (
         <>
-            <Card className="aspect-video mt-2 mb-6 shadow-none">
-                <CardCover>
-                    <video
-                        className="rounded-lg object-contain"
-                        playsInline
-                        muted={false}
-                        onClick={togglePlay}
-                        ref={vidRef}
-                        autoPlay={(play === 'play' || play === 'unknown')}
-                        loop
-                        poster="/video.png">
-                        {auth?.video ? (
-                            <source
-                                src={auth?.video}
-                                type="video/mp4" />
-                        ) : null}
-                    </video>
-                </CardCover>
-            </Card>
+            <video
+                className="aspect-video mt-2 mb-6 drop-shadow-xl rounded-lg object-contain"
+                playsInline
+                muted={false}
+                onClick={togglePlay}
+                ref={vidRef}
+                autoPlay={(play === 'play' || play === 'unknown')}
+                loop
+                poster="/video.png">
+                {auth?.video ? (
+                    <source
+                        src={auth?.video}
+                        type="video/mp4" />
+                ) : null}
+            </video>
         </>
     )
 }
