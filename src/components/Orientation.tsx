1f44a7d1feb37de6b74aef956239f6af133b2f97
souravdutta8012 <souravdutta2108@gmail.com>
2023-06-03 18:15:54 +0530

    changes

--- src/components/Orientation.tsx
+++ src/components/Orientation.tsx
@@ -1,16 +1,41 @@
+import { useState, useEffect } from "react";
 import { Box } from "@mui/material";
 
 export default function Orientation() {
+    const [compatibility, setCompatibility] = useState<boolean>(true);
+
+    useEffect(() => {
+        if (navigator.userAgent.match(/Windows/i)
+            || navigator.userAgent.match(/Macintosh/i)) {
+            setCompatibility(false);
+        } else {
+            setCompatibility(true);
+        }
+    }, []);
+
     return (
         <>
             <Box className="absolute w-full h-full bg-primary-gray">
                 <Box className="flex flex-col justify-center items-center w-full h-full gap-5">
-                    <Box>
-                        <img src={'/tilt.gif'} className="h-32" />
-                    </Box>
-                    <Box className="font-semibold text-xl">
-                        Please rotate your phone to portrait mode
-                    </Box>
+                    {compatibility ? (
+                        <>
+                            <Box>
+                                <img src={'/tilt.gif'} className="h-32" />
+                            </Box>
+                            <Box className="font-semibold text-xl">
+                                Please rotate your phone to portrait mode
+                            </Box>
+                        </>
+                    ) : (
+                        <>
+                            <Box>
+                                <img src={'/mobile.png'} className="h-32" />
+                            </Box>
+                            <Box className="font-semibold text-xl">
+                                Please open this website in mobile
+                            </Box>
+                        </>
+                    )}
                 </Box>
             </Box>
         </>
