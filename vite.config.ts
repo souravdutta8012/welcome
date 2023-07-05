3f80e24ab6122953571c03acde5ac6781d766c3c
souravdutta8012 <souravdutta2108@gmail.com>
2023-07-05 18:35:14 +0530

    changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,53 +1,56 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
-export default defineConfig({
-  plugins: [
-    VitePWA({
-      registerType: 'autoUpdate',
-      includeAssets: ['favicon.png'],
-      manifest: {
-        name: 'Welcome',
-        short_name: 'Welcome',
-        description: 'Welcome',
-        theme_color: '#1E2323',
-        background_color: '#1E2323',
-        display: "standalone",
-        lang: "en",
-        start_url: "/",
-        scope: "/",
-        id: "/",
-        icons: [
-          {
-            src: 'favicon.png',
-            sizes: '144x144',
-            type: 'image/png'
-          },
-          {
-            src: 'favicon.png',
-            sizes: '192x192',
-            type: 'image/png'
-          },
-          {
-            src: 'favicon.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any muskable'
-          }
-        ]
-      }
-    })
-  ],
-  build: {
-    outDir: './build'
-  },
-  define: {
-    'process.env': {}
-  },
-  resolve: {
-    alias: {
-      util: 'util/',
-      buffer: 'buffer/',
+export default ({ mode }) => {
+  process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+  return defineConfig({
+    plugins: [
+      VitePWA({
+        registerType: 'autoUpdate',
+        includeAssets: ['favicon.png'],
+        manifest: {
+          name: 'Welcome',
+          short_name: 'Welcome',
+          description: 'Welcome',
+          theme_color: '#1E2323',
+          background_color: '#1E2323',
+          display: "standalone",
+          lang: "en",
+          start_url: "/",
+          scope: "/",
+          id: "/",
+          icons: [
+            {
+              src: 'favicon.png',
+              sizes: '144x144',
+              type: 'image/png'
+            },
+            {
+              src: 'favicon.png',
+              sizes: '192x192',
+              type: 'image/png'
+            },
+            {
+              src: 'favicon.png',
+              sizes: '512x512',
+              type: 'image/png',
+              purpose: 'any muskable'
+            }
+          ]
+        }
+      })
+    ],
+    build: {
+      outDir: './build'
     },
-  }
-})
+    define: {
+      'process.env': process.env
+    },
+    resolve: {
+      alias: {
+        util: 'util/',
+        buffer: 'buffer/',
+      },
+    }
+  });
+}
